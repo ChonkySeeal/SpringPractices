90d2cf9481ef909b78b5ba49c7de5dc15f977c9d
ChonkySeeal <gsi0919@naver.com>
2022-08-29 11:50:15 +0900

    Tags and Data binding

---
+++ SpringMVCDemo/WebContent/WEB-INF/view/student-confirmation.jsp
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+The student is confirmed: ${student.firstName} ${student.lastName}
+
+
+</body>
+</html>
---
+++ SpringMVCDemo/src/spring/Student.java
@@ -0,0 +1,28 @@
+package spring;
+
+public class Student {
+
+	private String firstName;
+	private String lastName;
+
+	public Student() {
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstname) {
+		this.firstName = firstname;
+	}
+
+	public String getLastname() {
+		return lastName;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastName = lastname;
+	}
+
+}
