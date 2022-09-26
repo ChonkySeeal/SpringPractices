13b02c12a31e505fb302c844b7a9b646c052d579
ChonkySeeal <gsi0919@naver.com>
2022-09-26 18:38:14 +0900

    spring security logout feature is stuided

--- spring-security-demo-app-3-login-from/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-app-3-login-from/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -25,7 +25,7 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/showMyLoginPage")
-				.loginProcessingUrl("/authenticateTheUser").permitAll();
+				.loginProcessingUrl("/authenticateTheUser").permitAll().and().logout().permitAll();
 	}
 
 }
