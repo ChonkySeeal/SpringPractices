2f0e795b171a3b3450478c5410ae1bfbef492c6b
ChonkySeeal <gsi0919@naver.com>
2022-10-01 21:45:24 +0900

    SpringRest POJO to json and json to POJO is studied

---
+++ jackson-databinding-json-demo/src/main/java/json/config/MvcDispatcher.java
@@ -0,0 +1,25 @@
+package json.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcDispatcher extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+
+		return new Class[] { DemoAppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] { "/" };
+	}
+
+}
